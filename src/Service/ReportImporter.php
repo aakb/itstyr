7a8225fb6352c0b78048a593dc5b5afd03e05834
Troels Ugilt Jensen <tujensen@gmail.com>
2019-09-19 11:21:26 +0200

    DEVSUPP-39: Commented out broken import field

--- src/Service/ReportImporter.php
+++ src/Service/ReportImporter.php
@@ -50,7 +50,7 @@ class ReportImporter extends BaseImporter
             $report->setSysOwner($this->sanitizeText($entry->{'Systemejerskab'}));
             $report->setSysPurpose($this->sanitizeText($entry->{'Formål'}));
             $report->setSysClassification($this->sanitizeText($entry->{'Systemets klassifikation'}));
-            $report->setSysDateForRevision($this->convertDate($entry->{'Dato for revision'}));
+// @TODO:            $report->setSysDateForRevision($this->convertDate($entry->{'Dato for revision'}));
             $report->setSysPersons($this->sanitizeText($entry->{'Personkreds'}));
             $report->setSysInformationTypes($this->sanitizeText($entry->{'Oplysningstyper'}));
             $report->setSysDataSentTo($this->sanitizeText($entry->{'Hvor overføres data til?'}));
